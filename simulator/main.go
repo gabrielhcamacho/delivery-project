1504989da9a4d2312dd97149690ee067ce526f89
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2023-02-08 21:35:08 -0300

    kafka configured

--- simulator/main.go
+++ simulator/main.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
+	"encoding/json"
 
-	route2 "github.com/gabrielhcamacho/delivery-project/simulator/application/route"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+
+	kafka2 "github.com/gabrielhcamacho/delivery-project/simulator/application/kafka"
+	"github.com/gabrielhcamacho/delivery-project/simulator/infra/kafka"
 )
 
+	
+func init(){
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("error loading .env file")
+	}
+	
+}
 
 func main(){
 
-	route := route2.Route{
-		ID: "1",
-		ClientID: "1",
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
 	}
 
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[1])
+	// route := route2.Route{
+	// 	ID: "1",
+	// 	ClientID: "1",
+	// }
+
+	// route.LoadPositions()
+	// stringjson, _ := route.ExportJsonPositions()
+	// fmt.Println(stringjson[1])
 }
